742d5747adcf09b8f62df770e081f642bbe19c83
Vamshidhar Telugu <dev.vamshidhar@gmail.com>
2021-06-08 09:56:23 +0530

    fetch my profile api done

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,8 +1,24 @@
-const express = require('express');
+const express = require("express");
+const jwtVerify = require("../../middleware/jwtVerify");
 const router = express.Router();
+const Profile = require("../../models/Profile");
 
+router.get("/me", jwtVerify, async (req, res) => {
+  try {
+    const userId = req.user.id;
+    const profile = await Profile.findOne({ userId }).populate("user", [
+      "name",
+      "avatar",
+    ]);
+    if (!profile) {
+      return res.status(400).json({ msg: "No profile found for this user" });
+    }
 
-router.get('/', (req, res) => res.send("This is the profile router"));
+    return res.json(profile);
+  } catch (e) {
+    console.error(e);
+    return res.status(500).send("Server Error");
+  }
+});
 
-
-module.exports  = router;
+module.exports = router;
